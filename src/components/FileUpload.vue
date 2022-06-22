6627a2f0ce18c6ae41bcc90741c409a08086ff2d
StalvinAgile <stalvin.m@agileidc.com>
2022-06-22 10:14:16 +0530

    changed ui for upload section

--- src/components/FileUpload.vue
+++ src/components/FileUpload.vue
@@ -1,52 +1,33 @@
 <template>
   <v-app>
-    <div class="container">
-      <section id="image-form-wrapper">
-        <div class="container mt-5">
-          <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-              <div class="panel panel-default">
-                <!-- <v-card class="p-2 text-center">
-                <div class="panel-heading">
-                  <h5 class="panel-title">Upload Your File</h5>
-                </div>
-              </v-card> -->
-                <v-card class="pt-3 pb-1 px-2 mt-1">
-                  <form @submit.prevent="handleSubmit">
-                    <div class="form-group">
-                      <FileInput v-on:file-change="setFiles"> </FileInput>
-                    </div>
-                    <div class="form-group mt-2 d-flex justify-content-end">
-                      <v-tooltip bottom>
-                        <template v-slot:activator="{ on }">
-                          <a v-on="on">
-                          <v-btn
-                            color="success"
-                            
-                            type="submit"
-                            :disabled="!files || disableBtn"
-                          >
-                            Upload
-                          </v-btn>
-                          </a>
-                        </template>
-                        <span>Upload File</span>
-                      </v-tooltip>
-                    </div>
-                    <div class="mt-3">
-                      <ProgressBar
-                        :progress="progress"
-                        v-if="isUploading"
-                      ></ProgressBar>
-                    </div>
-                  </form>
-                </v-card>
-              </div>
-            </div>
+    
+      <v-card class="pt-3 pb-1 px-2 mt-1">
+        <form @submit.prevent="handleSubmit">
+          <div class="form-group">
+            <FileInput v-on:file-change="setFiles"> </FileInput>
           </div>
-        </div>
-      </section>
-    </div>
+          <div class="form-group mt-2 d-flex justify-content-end">
+            <v-tooltip bottom>
+              <template v-slot:activator="{ on }">
+                <a v-on="on">
+                  <v-btn
+                    color="success"
+                    type="submit"
+                    :disabled="!files || disableBtn"
+                  >
+                    Upload
+                  </v-btn>
+                </a>
+              </template>
+              <span>Upload File</span>
+            </v-tooltip>
+          </div>
+          <div class="mt-3">
+            <ProgressBar :progress="progress" v-if="isUploading"></ProgressBar>
+          </div>
+        </form>
+      </v-card>
+
   </v-app>
 </template>
 
@@ -78,7 +59,7 @@ export default {
           },
         })
         .then((res) => {
-          console.log('response ->',res);
+          console.log("response ->", res);
           setTimeout(() => {
             this.isUploading = false;
             this.progress = 0;
